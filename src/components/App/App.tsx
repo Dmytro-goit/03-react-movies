875bb49eeaa90817f78b9b2d935ac0b5735f4352
Dmytro-goit <dmytro.moiyseev@gmail.com>
2025-05-24 15:33:36 +0300

    -

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,11 +1,11 @@
 import { useState } from 'react';
 import type { Movie } from '../../types/movie';
 import { fetchMovies } from '../../services/movieService';
-import { SearchBar } from '../../SearchBar/SearchBar';
-import { MovieGrid } from '../../MovieGrid/MovieGrid';
-import { MovieModal } from '../../MovieModal/MovieModal';
-import { Loader } from '../../Loader/Loader';
-import { ErrorMessage } from '../../ErrorMessage/ErrorMessage';
+import { SearchBar } from '../SearchBar/SearchBar';
+import { MovieGrid } from '../MovieGrid/MovieGrid';
+import { MovieModal } from '../MovieModal/MovieModal';
+import { Loader } from '../Loader/Loader';
+import { ErrorMessage } from '../ErrorMessage/ErrorMessage';
 import { Toaster, toast } from 'react-hot-toast';
 import css from './App.module.css';
 import '/src/App.css';
