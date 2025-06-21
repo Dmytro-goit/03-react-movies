dff3f6f6cb01b537816115bf208bbcf0bb126ac5
Dmytro-goit <dmytro.moiyseev@gmail.com>
2025-06-21 22:37:39 +0300

    -

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -3,7 +3,7 @@ import type { Movie } from "../../types/movie";
 import { fetchMovies } from "../../services/movieService";
 import SearchBar from "../SearchBar/SearchBar";
 import { MovieGrid } from "../MovieGrid/MovieGrid";
-import { MovieModal } from "../MovieModal/MovieModal";
+import MovieModal from "../MovieModal/MovieModal";
 import { Loader } from "../Loader/Loader";
 import { ErrorMessage } from "../ErrorMessage/ErrorMessage";
 import { Toaster, toast } from "react-hot-toast";
--- src/components/SearchBar/SearchBar.tsx
+++ src/components/SearchBar/SearchBar.tsx
@@ -1,56 +1,40 @@
-import type { FormEvent } from "react";
-import toast from "react-hot-toast";
+import React from "react";
 import styles from "./SearchBar.module.css";
+import toast from "react-hot-toast";
 
 interface SearchBarProps {
   onSubmit: (query: string) => void;
 }
 
-const SearchBar = ({ onSubmit }: SearchBarProps) => {
-  const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
+export default function SearchBar({ onSubmit }: SearchBarProps) {
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
-
     const form = e.currentTarget;
     const formData = new FormData(form);
     const query = (formData.get("query") as string).trim();
 
     if (!query) {
-      toast.error("Please enter your search query.");
+      toast.error("Please enter a search query.");
       return;
     }
 
     onSubmit(query);
-    form.reset(); // Очищає input після сабміту
+    form.reset();
   };
 
   return (
     <header className={styles.header}>
-      <div className={styles.container}>
-        <a
-          className={styles.link}
-          href="https://www.themoviedb.org/"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by TMDB
-        </a>
-
-        <form className={styles.form} onSubmit={handleSubmit}>
-          <input
-            className={styles.input}
-            type="text"
-            name="query"
-            autoComplete="off"
-            placeholder="Search movies..."
-            autoFocus
-          />
-          <button className={styles.button} type="submit">
-            Search
-          </button>
-        </form>
-      </div>
+      <form className={styles.form} onSubmit={handleSubmit}>
+        <input
+          type="text"
+          name="query"
+          placeholder="Search movies..."
+          className={styles.input}
+        />
+        <button type="submit" className={styles.button}>
+          Search
+        </button>
+      </form>
     </header>
   );
-};
-
-export default SearchBar;
+}
